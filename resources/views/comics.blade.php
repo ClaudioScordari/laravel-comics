cbbb19a9271f052c86bde4053a910d74ca139391
ClaudioScordari <scordari22@gmail.com>
2024-02-25 15:30:39 +0100

    Completato esercizio di base - add footer - add main

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,15 +1,91 @@
 @extends('layouts.main-layout')
 
 @section('main-content')
-<div>
-    <ul>
-        @foreach ($comics as $oneComic)
-        <li>
-            <h3>
-                {{ $oneComic['title'] }}
-            </h3>
-        </li>
-        @endforeach
-    </ul>
-</div>
+
+@php
+$datasNav = [
+[
+'img' => '../images/buy-comics-digital-comics.png',
+'text'=> 'DIGITAL COMICS'
+],
+[
+'img' => '../images/buy-comics-merchandise.png',
+'text'=> 'DC MERCHANDISE'
+],
+[
+'img' => '../images/buy-comics-shop-locator.png',
+'text'=> 'COMIC SHOP LOCATOR'
+],
+[
+'img' => '../images/buy-comics-subscriptions.png',
+'text'=> 'SUBSCIPTIONS'
+],
+[
+'img' => '../images/buy-dc-power-visa.svg',
+'text'=> 'DC POWER VISA'
+]
+];
+@endphp
+
+<!-- Cards section -->
+<section>
+    <div class="jumbotron">
+        <!-- img background -->
+
+        <div>
+            <a href="">
+                CURRENT SERIES
+            </a>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="all-comics">
+
+            <!-- Card -->
+            @foreach ($comics as $oneComic)
+            <div class="one-comic">
+                <div class="img-comic">
+                    <img src="{{ $oneComic['thumb'] }}">
+                </div>
+
+                <br>
+
+                <div>
+                    {{ $oneComic['title'] }}
+                </div>
+            </div>
+            @endforeach
+
+        </div>
+
+        <div class="load_more-button">
+            <a href="">
+                LOAD MORE
+            </a>
+        </div>
+
+    </div>
+</section>
+
+<!-- Merch -->
+<section>
+    <div class="my-container">
+        <nav>
+            <ul>
+                @foreach ($datasNav as $link)
+                <li>
+                    <div>
+                        <img src="{{ $link['img'] }}">
+                    </div>
+
+                    <div>
+                        {{ $link['text'] }}
+                    </div>
+                </li>
+                @endforeach
+            </ul>
+        </nav>
+    </div>
+</section>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,271 @@
 <footer>
-    Footer
+    <section>
+        <div class="my-container">
+
+            <div class="col">
+                <h3>
+                    DC COMICS
+                </h3>
+
+                <br>
+
+                <ul>
+                    <li>
+                        <a href="">
+                            Characters
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Comics
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Movies
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Characters
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Comics
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Movies
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Movies
+                        </a>
+                    </li>
+                </ul>
+
+                <br>
+
+                <h3>
+                    SHOP
+                </h3>
+
+                <br>
+
+                <ul>
+                    <li>
+                        <a href="">
+                            Shop Dc
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Shop Dc Collectibles
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <h3>
+                    DC COMICS
+                </h3>
+
+                <br>
+
+                <ul>
+                    <li>
+                        <a href="">
+                            Characters
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Comics
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Movies
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Characters
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Comics
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Movies
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Movies
+                        </a>
+                    </li>
+                </ul>
+
+                <br>
+
+                <h3>
+                    SHOP
+                </h3>
+
+                <br>
+
+                <ul>
+                    <li>
+                        <a href="">
+                            Shop Dc
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Shop Dc Collectibles
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <h3>
+                    DC COMICS
+                </h3>
+
+                <br>
+
+                <ul>
+                    <li>
+                        <a href="">
+                            Characters
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Comics
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Movies
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Characters
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Comics
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Movies
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Movies
+                        </a>
+                    </li>
+                </ul>
+
+                <br>
+
+                <h3>
+                    SHOP
+                </h3>
+
+                <br>
+
+                <ul>
+                    <li>
+                        <a href="">
+                            Shop Dc
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            Shop Dc Collectibles
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <img src="../../images/dc-logo-bg.png" alt="">
+            </div>
+
+        </div>
+    </section>
+
+    <section>
+        <div class="my-container">
+
+            <div>
+                <a class="sign_up-button" href="">
+                    SIGN-UP NOW!
+                </a>
+            </div>
+
+            <div>
+                <h3>
+                    FOLLOW US
+                </h3>
+
+                <a href="">
+                    <img src="../../images/footer-facebook.png" alt="">
+                </a>
+
+                <a href="">
+                    <img src="../../images/footer-periscope.png" alt="">
+                </a>
+
+                <a href="">
+                    <img src="../../images/footer-pinterest.png" alt="">
+                </a>
+
+                <a href="">
+                    <img src="../../images/footer-twitter.png" alt="">
+                </a>
+
+                <a href="">
+                    <img src="../../images/footer-youtube.png" alt="">
+                </a>
+            </div>
+
+        </div>
+    </section>
 </footer>
